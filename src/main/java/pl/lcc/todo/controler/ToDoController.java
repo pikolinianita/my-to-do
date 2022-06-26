d8908fe82a2f68541771d440306af76072a98ed1
lcc <pikolinian@gmail.com>
2022-06-26 21:54:23 +0200

    FirstCommit

---
+++ src/main/java/pl/lcc/todo/controler/ToDoController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pl.lcc.todo.controler;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Nauczyciel
+ */
+@RestController
+public class ToDoController {
+    
+}
