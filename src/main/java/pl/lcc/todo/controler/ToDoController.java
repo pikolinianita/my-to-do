521716b65623c5bc6a9453425c1b952d8523a754
lcc <pikolinian@gmail.com>
2022-06-27 10:58:34 +0200

    FirstCommit

--- src/main/java/pl/lcc/todo/controler/ToDoController.java
+++ src/main/java/pl/lcc/todo/controler/ToDoController.java
@@ -4,6 +4,9 @@
  */
 package pl.lcc.todo.controler;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,6 +14,46 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Nauczyciel
  */
 @RestController
+@RequestMapping("/api")
 public class ToDoController {
     
+    @PostMapping
+   
+    String createProject(){
+        return "post Project";
+    }
+    
+    @PostMapping
+    
+    String createTask(){
+        return "post Task";
+    }
+    
+    @PostMapping
+    
+    String createEvent(){
+        return "post Event";
+    }
+    
+    @GetMapping
+    
+    String getProject(){
+        return "Projects";
+    }
+    
+    @GetMapping
+    
+    String getTask(){
+        return "Tasks";
+    }
+    
+    @GetMapping
+         
+    String getEvent(){
+        return "['Events']";
+    }
 }
+
+//fetch('https://jsonplaceholder.typicode.com/posts',{method: 'POST', headers: {'test': 'TestPost'} })
+//  .then(response => response.json())
+//  .then(json => console.log(json))
