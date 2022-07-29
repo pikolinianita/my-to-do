9692ca4867c59c4eb38f63b7a545c71e07788934
lcc <pikolinian@gmail.com>
2022-07-29 14:13:04 +0200

    cors

--- src/main/java/pl/lcc/todo/controler/ToDoController.java
+++ src/main/java/pl/lcc/todo/controler/ToDoController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import pl.lcc.todo.entities.UserDTO;
 @Slf4j
 @RestController
 @RequestMapping("/api")
+ @CrossOrigin
 public class ToDoController {
 
 RepoService repos;
