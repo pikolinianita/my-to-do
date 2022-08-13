225ffd0c554f36c6de7ef3da3bbc0d3244720f0a
pikolinian <pikolinian@gazeta.pl>
2022-08-13 20:49:38 +0200

    transfer

--- src/test/java/pl/lcc/todo/db/IntegrationTests.java
+++ src/test/java/pl/lcc/todo/db/IntegrationTests.java
@@ -8,9 +8,12 @@ import org.assertj.core.api.SoftAssertions;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -18,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author piko
  */
 @SpringBootTest
+@AutoConfigureMockMvc
 public class IntegrationTests {
    
   @Autowired
@@ -39,4 +43,18 @@ public class IntegrationTests {
     .andExpect(status().isOk());
     }
     
+    @Test
+    public void name2() throws Exception {
+      mockMvc.perform(post("/forums/42/register")
+    .contentType("application/json"))
+    .andExpect(status().isOk());
+    }
+    
+     @Test
+    public void name3() throws Exception {      
+        
+    mockMvc.perform(delete("/api/user/1")
+    .contentType("application/json"))
+    .andExpect(status().isOk());
+    }
 }
